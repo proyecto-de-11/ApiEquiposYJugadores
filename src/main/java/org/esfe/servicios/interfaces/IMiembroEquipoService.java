87686db6098b1e1788fcf5766a7a7a966f60ace7
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-11-15 16:44:52 -0600

    RC-134 Se agrego crearMembresiaDesdeInvitacion en el IMiembroEquipoService

--- src/main/java/org/esfe/servicios/interfaces/IMiembroEquipoService.java
+++ src/main/java/org/esfe/servicios/interfaces/IMiembroEquipoService.java
@@ -7,7 +7,6 @@ import org.esfe.dtos.miembro.MiembroSalidaDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface IMiembroEquipoService {
@@ -31,4 +30,6 @@ public interface IMiembroEquipoService {
 
     // 5. UTILIDAD
     boolean esMiembroExistente(Integer equipoId, Integer usuarioId);
+
+    MiembroSalidaDto crearMembresiaDesdeInvitacion(Integer equipoId, Integer usuarioId);
 }
