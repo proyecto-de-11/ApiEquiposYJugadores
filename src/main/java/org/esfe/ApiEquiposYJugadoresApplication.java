3f23895fff0e670beb025f3b87919a0ba5cc591b
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-11-21 20:28:10 -0600

    se conecta a eureka server

--- src/main/java/org/esfe/ApiEquiposYJugadoresApplication.java
+++ src/main/java/org/esfe/ApiEquiposYJugadoresApplication.java
@@ -2,8 +2,10 @@ package org.esfe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiEquiposYJugadoresApplication {
 
 	public static void main(String[] args) {
